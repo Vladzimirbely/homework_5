0041326c2b0a6db9ba4d06538bed484258429a2e
Uladzimir Bely <vladzimir.bely@gmail.com>
2024-03-21 21:20:05 +0300

    move width and height in fixture

--- test/test_demoqa.py
+++ test/test_demoqa.py
@@ -3,8 +3,6 @@ from selene import browser, be, have, command
 
 def test_enter_data():
     browser.open('/automation-practice-form')
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
     browser.element('#firstName').type('first name')
     browser.element('#lastName').type('last name')
     browser.element('#userEmail').type('test@test.test')
