86a0bd861aa2bed0acfaf4a6caee4b23c02cfd34
Uladzimir Bely <vladzimir.bely@gmail.com>
2024-03-21 15:17:12 +0300

    add test for first name, last name and email

--- test/test_demoqa.py
+++ test/test_demoqa.py
@@ -4,3 +4,7 @@ import pytest
 def test_check_name_header():
     browser.element('h1').should(have.text('Practice Form'))
 
+def test_enter_data():
+    browser.element('#firstName').should(be.blank).type(('first name'))
+    browser.element('#lastName').should(be.blank).type(('last name'))
+    browser.element('#userEmail').should(be.blank).type(('test@test.test'))
