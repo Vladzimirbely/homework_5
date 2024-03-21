12a1cf0ee5b83d375c5dce8c126dd75c7b45de09
Uladzimir Bely <vladzimir.bely@gmail.com>
2024-03-21 16:08:55 +0300

    add tests for gender, mobilenumber adn date of birth

--- test/test_demoqa.py
+++ test/test_demoqa.py
@@ -1,5 +1,4 @@
 from selene import browser, be, have
-import pytest
 
 def test_check_name_header():
     browser.element('h1').should(have.text('Practice Form'))
@@ -8,3 +7,10 @@ def test_enter_data():
     browser.element('#firstName').should(be.blank).type(('first name'))
     browser.element('#lastName').should(be.blank).type(('last name'))
     browser.element('#userEmail').should(be.blank).type(('test@test.test'))
+    browser.element('[for="gender-radio-2"]').should(be.visible).click()
+    browser.element('.react-datepicker-wrapper').click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.element('option[value="2"]').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('option[value="1998"]').click()
+    browser.element('.react-datepicker__day--002').click()
