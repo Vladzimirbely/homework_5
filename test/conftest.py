67e7e4bbc2b470b4e32fe9de61f64b8184da3780
Uladzimir Bely <vladzimir.bely@gmail.com>
2024-03-21 19:58:50 +0300

    fix all comments

--- test/conftest.py
+++ test/conftest.py
@@ -3,8 +3,7 @@ from selene import browser, be, have
 
 @pytest.fixture(scope='function', autouse=True)
 def open_browser():
-    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
-    browser.open('/')
+    browser.config.base_url = 'https://demoqa.com'
 
     yield
 
--- test/test_demoqa.py
+++ test/test_demoqa.py
@@ -1,27 +1,27 @@
-from selene import browser, be, have
-
-def test_check_name_header():
-    browser.element('h1').should(have.text('Practice Form'))
+import os
+from selene import browser, be, have, command
 
 def test_enter_data():
-    browser.element('#firstName').should(be.blank).type(('first name'))
-    browser.element('#lastName').should(be.blank).type(('last name'))
-    browser.element('#userEmail').should(be.blank).type(('test@test.test'))
-    browser.element('#userNumber').should(be.blank).type('1234567890')
-    browser.element('[for="gender-radio-2"]').should(be.visible).click()
+    browser.open('/automation-practice-form')
+    browser.driver.set_window_size(1920, 1500)
+    browser.element('#firstName').type('first name')
+    browser.element('#lastName').type('last name')
+    browser.element('#userEmail').type('test@test.test')
+    browser.element('#userNumber').type('1234567890')
+    browser.element('[for="gender-radio-2"]').click()
     browser.element('.react-datepicker-wrapper').click()
     browser.element('.react-datepicker__month-select').click()
     browser.element('option[value="2"]').click()
-    browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
     browser.element('.react-datepicker__year-select').click()
     browser.element('option[value="1998"]').click()
     browser.element('.react-datepicker__day--002').click()
     browser.element('#subjectsInput').type('Computer Science').press_enter()
     browser.element('label[for="hobbies-checkbox-3"]').click()
-    browser.element('#uploadPicture').send_keys('C:/Users/vladz/OneDrive/Desktop/homework_5/test/images/Screenshot_1.png')
-    browser.element('#currentAddress').should(be.blank).type('Address')
+    browser.element('#uploadPicture').send_keys(os.path.abspath('images/Screenshot_1.png'))
+    browser.element('#currentAddress').type('Address')
     browser.element('#react-select-3-input').type('Haryana').press_enter()
     browser.element('#react-select-4-input').type('Karnal').press_enter()
+    browser.element('#submit').perform(command.js.scroll_into_view)
     browser.element('#submit').click()
 
     browser.element('.table').all('tr td:nth-child(2)').should(have.texts
